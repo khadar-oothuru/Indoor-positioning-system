d5ca236050d2a6d11b8eff6cbad95a5cfaf23883
khadar-oothuru <khadaroothuru@gmail.com>
2024-05-01 11:11:03 +0530

    updated scanble

--- src/components/ScanBLE.jsx
+++ src/components/ScanBLE.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Nav from "./Nav";
 import { FaArrowCircleLeft } from "react-icons/fa";
+import { toast, ToastContainer } from "react-toastify"; // Import toast and ToastContainer
+import "react-toastify/dist/ReactToastify.css"; // Import toast styles
 import "../components/styles/ScanBLE.css";
 
 const ScanBLEPage = () => {
@@ -14,24 +16,27 @@ const ScanBLEPage = () => {
       const options = {
         acceptAllDevices: true, // Accept all BLE devices
       };
-      
+
       const devices = await navigator.bluetooth.requestDevice(options);
-      
+
       // Extract device information
-      const scannedDevicesData = devices.map(device => ({
+      const scannedDevicesData = devices.map((device) => ({
         id: device.id,
         name: device.name,
       }));
-      
+
       setScannedDevices(scannedDevicesData);
+      toast.success("Scanning complete!"); // Display toast message
     } catch (error) {
       console.error("Error scanning BLE devices:", error);
+      toast.error("Error scanning BLE devices"); // Display error toast message
     }
   };
 
   return (
     <>
       <Nav />
+      <ToastContainer /> {/* Place ToastContainer at top level of your component */}
       <div className="scan-ble-page">
         <Link to="/" className="back-link-scan">
           <FaArrowCircleLeft className="back-icon-scan" />
